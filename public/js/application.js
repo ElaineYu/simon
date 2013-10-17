195a97ef56a131d8819c83e1b46bff37c38b93b5
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2013-10-17 09:51:45 -0500

    Click event works, but colors don't

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
+	$( "#get_color" ).click(function( event ) {
+    	console.log('submission');
+    	 //prevent the default action for that event from happening
+    	event.preventDefault();
+    	// generate random color? color_me
+    	$.post('/color', function(response) {
+    		//use jQuery to submit an AJAX post to the form's action
+    		$("#color_me").html(response);
+    
+    });
+  });
 });
---
+++ vendor/bundle/gems/pg-0.15.1/ext/siteconf20131017-3025-3vidxh.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20131017-3025-wa3hza"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
